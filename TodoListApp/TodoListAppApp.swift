75a81be4fc7ba3b70e2d1dbcabc07abd047d07d7
Aurelie <3kdapp25.vaudau@folkuniversitetet.nu>
2026-03-16 11:58:33 +0100

    Initial Commit

---
+++ TodoListApp/TodoListAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoListAppApp.swift
+//  TodoListApp
+//
+//  Created by Aurelie Vaudan APP25 on 2026-03-16.
+//
+
+import SwiftUI
+
+@main
+struct TodoListAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
